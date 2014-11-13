aabde56209e6f42fc1006c8f23bfa6ec005548c2
Anton Podviaznikov <podviaznikov@gmail.com>
2014-11-13 14:21:11 -0800

    add debug and morgan deps. minor cleanups

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict';
 require('loadenv.js')();
 var app = require('./lib/app.js');
+var debug = require('debug')('docker-listener:server');
 
 app.listen(process.env.PORT);
-console.log('server listen on', process.env.PORT);
+debug('server listen on', process.env.PORT);
