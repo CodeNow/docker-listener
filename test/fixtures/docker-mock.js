fc58ae48f598a340489a0b7753681a5f4d477c9f
Casey Flynn <cflynn.us@gmail.com>
2015-07-17 14:21:11 -0700

    removing debug

--- test/fixtures/docker-mock.js
+++ test/fixtures/docker-mock.js
@@ -1,11 +1,10 @@
 'use strict';
 var spawn = require('child_process').fork;
-var debug = require('auto-debug')();
 
 module.exports.start = function (cb) {
   this.server = spawn('test/fixtures/docker-server.js');
 
-  debug('Spawned child pid: ', this.server.pid);
+  console.log('Spawned child pid: ', this.server.pid);
   this.server.on('message', function (msg) {
     if (msg === 'started') {
       cb();
@@ -16,7 +15,7 @@ module.exports.start = function (cb) {
 module.exports.stop = function (cb) {
   if (this.server) {
     this.server.on('exit', function (code) {
-      debug('docker server process exited with code ', code);
+      console.log('docker server process exited with code ', code);
       cb();
     });
     this.server.kill('SIGHUP');
