0f6418e1969ea547b5760c73daf123ed10f5736f
Anton Podviaznikov <podviaznikov@gmail.com>
2015-12-25 11:59:48 -0800

    adding unit tests

--- test/worker-docker.event.publish.js
+++ test/worker-docker.event.publish.js
@@ -11,6 +11,7 @@ var Lab = require('lab');
 var DockerEventPublish = require('../lib/workers/docker.event.publish.js');
 var sinon = require('sinon');
 var ip = require('ip');
+var os = require('os');
 
 var lab = exports.lab = Lab.script();
 
@@ -23,13 +24,14 @@ var it = lab.test;
 
 describe('docker event publish', function () {
   describe('_createRoutingKey', function () {
+    var originalHostTags = process.env.HOST_TAGS
     beforeEach(function (done) {
       process.env.HOST_TAGS = 'testOrg,run.build';
       done();
     });
 
     afterEach(function (done) {
-      delete process.env.HOST_TAGS;
+      process.env.HOST_TAGS = originalHostTags
       done();
     });
 
@@ -39,5 +41,38 @@ describe('docker event publish', function () {
       done();
     });
   });
+  describe('_addBasicFields', function () {
+    it('should add ip, uuid, host, time', function (done) {
+      var original = {
+        id: 'some-id'
+      };
+      var currDate = Date.now();
+      var enhanced = DockerEventPublish._addBasicFields(original)
+      expect(enhanced.time).to.be.at.least(currDate);
+      expect(enhanced.uuid).to.exist();
+      expect(enhanced.ip).to.equal(ip.address());
+      var host = 'http://' + ip.address() + ':' + process.env.DOCKER_REMOTE_API_PORT;
+      expect(enhanced.host).to.equal(host);
+      done();
+    });
+
+    it('should not change time if exist', function (done) {
+      var original = {
+        id: 'some-id',
+        time: Date.now() - 1000
+      };
+      var enhanced = DockerEventPublish._addBasicFields(original)
+      expect(enhanced.time).to.equal(original.time);
+      expect(enhanced.uuid).to.exist();
+      expect(enhanced.ip).to.equal(ip.address());
+      expect(enhanced.numCpus).to.equal(os.cpus().length);
+      expect(enhanced.mem).to.equal(os.totalmem());
+      expect(enhanced.tags).to.equal(process.env.HOST_TAGS);
+
+      var host = 'http://' + ip.address() + ':' + process.env.DOCKER_REMOTE_API_PORT;
+      expect(enhanced.host).to.equal(host);
+      done();
+    });
+  })
 
 });
