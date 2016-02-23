9952a1039ce41d2cbfde6e42c1cf8cdfda042ef2
Anandkumar Patel <anand@runnable.com>
2016-02-22 16:52:53 -0800

    remove route key, make new test for listener

---
+++ test/fixtures/event-mock.js
@@ -0,0 +1,19 @@
+module.exports = function (opts) {
+  opts = opts || {}
+  var ip = opts.ip || '10.1.1.1'
+  var hIp = ip.replace(/\./g, '-')
+  var org = opts.org || '123123123'
+
+  return {
+    status: opts.status || 'start',
+    id: opts.id || '74cef3acf07a3b1171557d24a229dd67bdddd53a84e42c14295ebf72434e0676',
+    from: (opts.from || 'ubuntu') + ' node:ip-' + hIp + '--' + org,
+    time: opts.time || (Date.now() / 1000).toFixed(0),
+    node: {
+      Name: 'ip-' + hIp + '--' + org,
+      Id: 'U4AO:ZZAG:VJFC:PVDZ:LPUM:FNAC:UV7J:3KUH:UIYT:MKTM:TGA7:CPOI',
+      Addr: ip + ':4242',
+      Ip: ip
+    }
+  }
+}
---
+++ test/unit/listener.js
@@ -0,0 +1,237 @@
+/**
+ * @module test/listener
+ */
+'use strict'
+
+require('loadenv')({ debugName: 'docker-listener' })
+var Code = require('code')
+var Lab = require('lab')
+var sinon = require('sinon')
+var EventEmitter = require('events').EventEmitter
+var ErrorCat = require('error-cat')
+
+var Listener = require('../../lib/listener')
+var docker = require('../../lib/docker')
+var datadog = require('../../lib/datadog')
+
+var lab = exports.lab = Lab.script()
+
+var afterEach = lab.afterEach
+var beforeEach = lab.beforeEach
+var describe = lab.experiment
+var expect = Code.expect
+var it = lab.test
+
+describe('listener unit test', function () {
+  describe('constructor', function () {
+    it('should throw if invalid publisher', function (done) {
+      expect(function () {
+        new Listener({})
+      }).to.throw(Error)
+      done()
+    })
+
+    it('should setup listener', function (done) {
+      var listener
+      var testPub = { write: 'hi' }
+      expect(function () {
+        listener = new Listener(testPub)
+      }).to.not.throw()
+      expect(listener.publisher).to.deep.equal(testPub)
+      expect(listener.dockerEventStream).to.be.null()
+      expect(listener.manualClose).to.be.false()
+      expect(listener).to.be.an.instanceOf(EventEmitter)
+      done()
+    })
+  }) // end constructor
+
+  describe('methods', function () {
+    var listener
+    beforeEach(function (done) {
+      listener = new Listener({ write: sinon.stub() })
+      done()
+    })
+
+    describe('start', function () {
+      beforeEach(function (done) {
+        sinon.stub(docker, 'getEvents')
+        done()
+      })
+
+      afterEach(function (done) {
+        docker.getEvents.restore()
+        done()
+      })
+
+      it('should reconnect on error', function (done) {
+        docker.getEvents.yieldsAsync('error')
+        sinon.stub(listener, 'reconnect', function () {
+          sinon.assert.calledOnce(docker.getEvents)
+          sinon.assert.calledWith(docker.getEvents, sinon.match.func)
+          done()
+        })
+
+        listener.start()
+      })
+
+      it('should reconnect on error', function (done) {
+        docker.getEvents.yieldsAsync('error')
+        sinon.stub(listener, 'reconnect', function () {
+          sinon.assert.calledOnce(docker.getEvents)
+          sinon.assert.calledWith(docker.getEvents, sinon.match.func)
+          done()
+        })
+
+        listener.start()
+      })
+
+      it('should setup pipes', function (done) {
+        var stubStream = {
+          on: sinon.stub().returnsThis(),
+          pipe: sinon.stub(),
+          socket: {
+            setTimeout: sinon.stub()
+          }
+        }
+        docker.getEvents.yieldsAsync(null, stubStream)
+        sinon.stub(listener, 'emit', function (name) {
+          expect(name).to.equal('started')
+
+          sinon.assert.calledTwice(stubStream.on)
+          sinon.assert.calledWith(stubStream.on, 'error', sinon.match.func)
+          sinon.assert.calledWith(stubStream.on, 'close', sinon.match.func)
+
+          sinon.assert.calledTwice(stubStream.pipe)
+          sinon.assert.calledWith(stubStream.pipe, listener.publisher)
+          done()
+        })
+
+        listener.start()
+      })
+    }) // end start
+
+    describe('stop', function () {
+      beforeEach(function (done) {
+        listener.dockerEventStream = {
+          destroy: sinon.stub()
+        }
+        done()
+      })
+
+      it('should call destroy', function (done) {
+        listener.dockerEventStream.destroy.returns()
+
+        sinon.stub(listener, 'emit', function (name) {
+          expect(name).to.equal('stopped')
+          expect(listener.manualClose).to.be.true()
+          sinon.assert.calledOnce(listener.dockerEventStream.destroy)
+          done()
+        })
+
+        listener.stop()
+      })
+
+      it('should not call destroy', function (done) {
+        delete listener.dockerEventStream
+        listener.manualClose = false
+
+        sinon.stub(listener, 'emit', function (name) {
+          expect(name).to.equal('stopped')
+          expect(listener.manualClose).to.be.false()
+          done()
+        })
+
+        listener.stop()
+      })
+    }) // end stop
+
+    describe('handleError', function () {
+      beforeEach(function (done) {
+        sinon.stub(datadog, 'inc')
+        sinon.stub(ErrorCat.prototype, 'createAndReport')
+        done()
+      })
+
+      afterEach(function (done) {
+        datadog.inc.restore()
+        ErrorCat.prototype.createAndReport.restore()
+        done()
+      })
+
+      it('should call reporting tools', function (done) {
+        var err = 'booms'
+        listener.handleError(err)
+        sinon.assert.calledOnce(datadog.inc)
+        sinon.assert.calledWith(datadog.inc, 'error')
+
+        sinon.assert.calledOnce(ErrorCat.prototype.createAndReport)
+        sinon.assert.calledWith(ErrorCat.prototype.createAndReport, 404, 'Docker streaming error', err)
+        done()
+      })
+    }) // end handleError
+
+    describe('handleClose', function () {
+      var destroyStub = sinon.stub()
+      beforeEach(function (done) {
+        listener.dockerEventStream = {
+          destroy: destroyStub
+        }
+        sinon.stub(listener, 'reconnect')
+        done()
+      })
+
+      it('should not call destroy or reconnect', function (done) {
+        listener.manualClose = true
+        listener.handleClose()
+
+        sinon.assert.notCalled(destroyStub)
+        done()
+      })
+
+      it('should call destroy and reconnect', function (done) {
+        listener.manualClose = false
+        listener.handleClose()
+
+        expect(listener.manualClose).to.be.false()
+        expect(listener.dockerEventStream).to.be.null()
+        sinon.assert.calledOnce(destroyStub)
+        sinon.assert.calledOnce(listener.reconnect)
+        done()
+      })
+    }) // end handleClose
+
+    describe('reconnect', function () {
+      var clock
+      beforeEach(function (done) {
+        clock = sinon.useFakeTimers()
+        sinon.stub(listener, 'start')
+        process.env.AUTO_RECONNECT = 'true'
+        process.env.DOCKER_REMOTE_API_RETRY_INTERVAL = 10
+        done()
+      })
+
+      afterEach(function (done) {
+        clock.restore()
+        delete process.env.AUTO_RECONNECT
+        delete process.env.DOCKER_REMOTE_API_RETRY_INTERVAL
+        done()
+      })
+
+      it('should not reconnect', function (done) {
+        delete process.env.AUTO_RECONNECT
+
+        listener.reconnect()
+        clock.tick(100)
+        sinon.assert.notCalled(listener.start)
+        done()
+      })
+
+      it('should call reconnect', function (done) {
+        listener.reconnect()
+        clock.tick(100)
+        sinon.assert.calledOnce(listener.start)
+        done()
+      })
+    }) // end reconnect
+  }) // end methods
+})
