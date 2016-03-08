9f6bf71d0a97ee4dd31f1adafc50f2548e76a00a
Anandkumar Patel <anand@runnable.com>
2016-03-07 16:47:00 -0800

    add more test

--- test/unit/listener.js
+++ test/unit/listener.js
@@ -11,6 +11,7 @@ var sinon = require('sinon')
 
 var docker = require('../../lib/docker')
 var Listener = require('../../lib/listener')
+var rabbitmq = require('../../lib/rabbitmq')
 
 var lab = exports.lab = Lab.script()
 
@@ -47,6 +48,7 @@ describe('listener unit test', function () {
         sinon.stub(docker, 'getEvents')
         sinon.stub(ErrorCat.prototype, 'createAndReport')
         sinon.stub(listener, 'handleClose')
+        sinon.stub(listener, 'testEvent')
         done()
       })
 
@@ -55,6 +57,7 @@ describe('listener unit test', function () {
         ErrorCat.prototype.createAndReport.restore()
         listener.handleClose.restore()
         clock.restore()
+        listener.testEvent.restore()
         delete process.env.EVENT_TIMEOUT_MS
         done()
       })
@@ -76,6 +79,7 @@ describe('listener unit test', function () {
           once: sinon.stub().returnsThis()
         }
         docker.getEvents.yieldsAsync(null, stubStream)
+        listener.testEvent.yieldsAsync()
 
         listener.start(function () {
           sinon.assert.callCount(stubStream.on, 3)
@@ -94,6 +98,7 @@ describe('listener unit test', function () {
           on: sinon.stub().returnsThis(),
           once: sinon.stub().returnsThis()
         }
+        listener.testEvent.yieldsAsync()
         docker.getEvents.yieldsAsync(null, stubStream)
 
         listener.start(function () {
@@ -111,6 +116,7 @@ describe('listener unit test', function () {
           once: sinon.stub().yields()
         }
         docker.getEvents.yieldsAsync(null, stubStream)
+        listener.testEvent.yieldsAsync()
 
         listener.start(function () {
           clock.tick(200)
@@ -164,5 +170,104 @@ describe('listener unit test', function () {
         done()
       })
     }) // end handleClose
+
+    describe('testEvent', function () {
+      var topMock = {
+        top: function () { }
+      }
+      beforeEach(function (done) {
+        sinon.stub(docker, 'listContainers')
+        sinon.stub(topMock, 'top')
+        sinon.stub(docker, 'getContainer').returns(topMock)
+        sinon.stub(ErrorCat.prototype, 'createAndReport')
+        done()
+      })
+
+      afterEach(function (done) {
+        docker.listContainers.restore()
+        docker.getContainer.restore()
+        topMock.top.restore()
+        ErrorCat.prototype.createAndReport.restore()
+        done()
+      })
+
+      it('should report error on list fail', function (done) {
+        var testErr = 'calamitous'
+        docker.listContainers.yieldsAsync(testErr)
+        listener.testEvent(function (err) {
+          if (err) { return done(err) }
+
+          sinon.assert.calledOnce(ErrorCat.prototype.createAndReport)
+          sinon.assert.calledWith(ErrorCat.prototype.createAndReport, 500, 'failed to list containers', testErr)
+          done()
+        })
+      })
+
+      it('should report error on empty containers', function (done) {
+        docker.listContainers.yieldsAsync(null, [])
+        listener.testEvent(function (err) {
+          if (err) { return done(err) }
+
+          sinon.assert.calledOnce(ErrorCat.prototype.createAndReport)
+          sinon.assert.calledWith(ErrorCat.prototype.createAndReport, 404, 'no running containers found')
+          done()
+        })
+      })
+
+      it('should report error top fail', function (done) {
+        var testErr = 'grievous'
+        docker.listContainers.yieldsAsync(null, [{Id: 1}])
+        topMock.top.yieldsAsync(testErr)
+        listener.testEvent(function (err) {
+          if (err) { return done(err) }
+
+          sinon.assert.calledOnce(ErrorCat.prototype.createAndReport)
+          sinon.assert.calledWith(ErrorCat.prototype.createAndReport, 500, 'failed to run top', testErr)
+          done()
+        })
+      })
+
+      it('should callback', function (done) {
+        var testId = 'heinous'
+        docker.listContainers.yieldsAsync(null, [{Id: testId}])
+        topMock.top.yieldsAsync()
+        listener.testEvent(function (err) {
+          if (err) { return done(err) }
+          sinon.assert.calledOnce(docker.listContainers)
+          sinon.assert.calledOnce(docker.getContainer)
+          sinon.assert.calledWith(docker.getContainer, testId)
+          sinon.assert.calledOnce(topMock.top)
+          sinon.assert.notCalled(ErrorCat.prototype.createAndReport)
+          done()
+        })
+      })
+    }) // end testEvent
+
+    describe('publishEvent', function () {
+      beforeEach(function (done) {
+        sinon.stub(rabbitmq, 'createPublishJob')
+        done()
+      })
+
+      afterEach(function (done) {
+        rabbitmq.createPublishJob.restore()
+        done()
+      })
+
+      it('should publish event', function (done) {
+        var testEvent = { type: 'abhorrent' }
+        listener.publishEvent(testEvent)
+
+        sinon.assert.calledOnce(rabbitmq.createPublishJob)
+        sinon.assert.calledWith(rabbitmq.createPublishJob, testEvent)
+        done()
+      })
+
+      it('should not publish event', function (done) {
+        listener.publishEvent()
+        sinon.assert.notCalled(rabbitmq.createPublishJob)
+        done()
+      })
+    }) // end publishEvent
   }) // end methods
 })
