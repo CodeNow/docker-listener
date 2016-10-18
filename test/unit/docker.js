c6198f0a214679c5927daa6abfc6ee491e2f24a4
Anton Podviaznikov <podviaznikov@gmail.com>
2016-10-18 16:08:39 -0700

    use loki docker utils

--- test/unit/docker.js
+++ test/unit/docker.js
@@ -33,27 +33,4 @@ describe('docker unit test', () => {
       done()
     })
   })
-  describe('toDockerHost', () => {
-    it('should convert url to host', (done) => {
-      expect(Docker.toDockerHost('http://10.0.0.1:4242')).to.equal('10.0.0.1:4242')
-      done()
-    })
-
-    it('should return same valid host', (done) => {
-      expect(Docker.toDockerHost('10.0.0.1:4242')).to.equal('10.0.0.1:4242')
-      done()
-    })
-  })
-
-  describe('toDockerUrl', () => {
-    it('should convert host to url', (done) => {
-      expect(Docker.toDockerUrl('10.0.0.1:4242')).to.equal('http://10.0.0.1:4242')
-      done()
-    })
-
-    it('should return same valid url', (done) => {
-      expect(Docker.toDockerUrl('http://10.0.0.1:4242')).to.equal('http://10.0.0.1:4242')
-      done()
-    })
-  })
 })
