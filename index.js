81b84a6d3631f379029ed22405f66500f45fca00
Anandkumar Patel <anand@runnable.com>
2017-06-27 15:28:51 -0700

    fix this

--- index.js
+++ index.js
@@ -6,13 +6,14 @@ const log = require('./lib/logger')()
 //TODO: hack
 const dgram = require('dgram')
 const originalCreateSocket = dgram.createSocket
-dgram.createSocket = function (type, a, b, c, d, f, g) {
+dgram.createSocket = function () {
   log.warn({ stack: new Error('stack check createSocket').stack }, 'XXXX createSocket')
-  const out = originalCreateSocket(type, a, b, c, d, f, g)
+  const out = originalCreateSocket.apply(this, arguments)
   const oldSend = out.send
-  out.send = function (a,b,c,d,e,f,g,h,i) {
+
+  out.send = function () {
     log.warn({ stack: new Error('stack check send').stack }, 'XXXX send')
-    return oldSend(a,b,c,d,e,f,g,h,i)
+    return oldSend.apply(this, arguments)
   }
   return out
 }
